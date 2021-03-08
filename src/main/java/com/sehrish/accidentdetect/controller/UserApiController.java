37d24a4b07cf5760e7ac32664948930a9e875071
Sehrish Fahim <sehrish.fahim91@gmail.com>
2021-03-08 23:23:30 +0100

    fix jquery

--- src/main/java/com/sehrish/accidentdetect/controller/UserApiController.java
+++ src/main/java/com/sehrish/accidentdetect/controller/UserApiController.java
@@ -1,19 +1,25 @@
 package com.sehrish.accidentdetect.controller;
 
-import com.sehrish.accidentdetect.dto.RemoveContactDto;
-import com.sehrish.accidentdetect.dto.UserDto;
-import com.sehrish.accidentdetect.dto.UserFriendDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pusher.rest.Pusher;
+import com.sehrish.accidentdetect.dto.*;
+import com.sehrish.accidentdetect.entity.Hospital;
 import com.sehrish.accidentdetect.entity.User;
 import com.sehrish.accidentdetect.entity.UserFamilyContact;
 import com.sehrish.accidentdetect.repository.UserFriendRepository;
 import com.sehrish.accidentdetect.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -88,6 +94,13 @@ public class UserApiController {
 
     @PostMapping("/friendslist")
     public List<UserFamilyContact> getfriendinfo(@RequestBody UserDto userDto) {
+
+        Pusher pusher = new Pusher("1168496", "a9e662200a7506256f55", "a473edae383211e0f7dc");
+        pusher.setCluster("eu");
+        pusher.setEncrypted(true);
+
+        pusher.trigger("my-channel", "my-event", Collections.singletonMap("message", userDto.getName()));
+
         User user = userRepository.findById(userDto.getId());
         return user.getUserFamilyContacts();
     }
